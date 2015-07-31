8ca97e99a39e59e59e27d9d3481927790a6032e3
Antoine Sanchez <ckkoshi@gmail.com>
2015-07-31 16:37:37 +0200

    clean file + solve defect
    Possible to trigger close that will close only the good target

--- toggler.js
+++ toggler.js
@@ -15,11 +15,13 @@ var selector = function (e) {
     var closing = action === 'close';
 
     var $linkToggler = $(this);
-    var group = $linkToggler.data('accordion-group');
-    var toggler_id = $linkToggler.data('accordion-id');
-    var $content = $('.JS_item_accordion').filter('[data-accordion-itemid=' + toggler_id + '][data-accordion-group=' + group + ']');
-    var $linksTogglerGroup = $allLinksToggler.filter('[data-accordion-itemid=' + toggler_id + '][data-accordion-group=' + group + ']');
-    var $contentGroup = $('[data-accordion-group=' + group + ']');
+    var $allContents = $('.JS_item_toggler');
+
+    var group = $linkToggler.data('toggler-group');
+    var toggler_id = $linkToggler.data('toggler-id');
+    var $content = $allContents.filter('[data-toggler-itemid=' + toggler_id + '][data-toggler-group=' + group + ']');
+    var $linksTogglerGroup = $allLinksToggler.filter('[data-toggler-itemid=' + toggler_id + '][data-toggler-group=' + group + ']');
+    var $contentGroup = closing ? $content : $allContents.filter('[data-toggler-group=' + group + ']');
 
     var isActive = opening ? !opening : closing ? closing : $content.hasClass('active');
 
