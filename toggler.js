428f316e04ea597299fd2a85dd4ac28fb0b1ab04
antsan2 <ckkoshi@gmail.com>
2016-03-22 17:16:51 +0100

    fix stack overflow + add close all
    toggler.js :
    * update with specific event to avoid stack overflow while having the content inside the trigger
    * add close all data

--- toggler.js
+++ toggler.js
@@ -1,40 +1,38 @@
-/**
- * Created by stecov on 26/01/2015.
- * @version 1.0
- */
-
 var togglers = (function () {
     var $allLinksToggler;
     var selector = function (e) {
 
+        e.stopImmediatePropagation();//todo check how to improve this quick fix
+        var $allLinksToggler = $('.JS_toggler');
         var $linkToggler = $(this);
-        var $allContents = $('.JS_item_toggler');
-        var $linksTogglerGroup;
-
         var action = e.type;
         var toggle = /click|toggle/.test(action);
         var opening = action === 'open' || $linkToggler.data('toggler-action') === 'open';
         var closing = action === 'close' || $linkToggler.data('toggler-action') === 'close';
+        var closeAll = action === 'close-all' || $linkToggler.data('toggler-action') === 'close-all';
 
+        var $allContents = $('.JS_item_toggler');
         var group = $linkToggler.data('toggler-group');
         var toggler_id = $linkToggler.data('toggler-id');
         var $content = $allContents.filter('[data-toggler-itemid=' + toggler_id + '][data-toggler-group=' + group + ']');
         var $contentGroup = closing ? $content : $allContents.filter('[data-toggler-group=' + group + ']');
+
         var isActive = opening ? !opening : closing ? closing : $content.hasClass('active');
 
         // Add remove classes
-        if($content.data('toggler-group-no-close') && !((toggle || opening || closing) && !isActive)){
+        if ($content.data('toggler-group-no-close') && !((toggle || opening || closing) && !isActive)) {
             return;
         }
-        if (toggle || opening || closing) {
+        var $linksTogglerGroup;
+        if (toggle || opening || closing || closeAll) {
             $linksTogglerGroup = $allLinksToggler.filter('[data-toggler-group=' + group + ']');
             $linksTogglerGroup.removeClass('active');
-            $contentGroup.filter('.active').removeClass('active').trigger('close');
+            $contentGroup.filter('.active').removeClass('active').trigger('close.content');
         }
-        if (!isActive) {
+        if (!isActive && !closeAll) {
             $linksTogglerGroup = $allLinksToggler.filter('[data-toggler-id=' + toggler_id + '][data-toggler-group=' + group + ']');
             $linksTogglerGroup.addClass('active');
-            $content.addClass('active').trigger('open');
+            $content.addClass('active').trigger('open.content');
         }
     };
 
